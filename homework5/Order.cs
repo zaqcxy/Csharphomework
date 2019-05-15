b0dfefc26ea4ddecea2e74759762ab72431eb92c
zaqcxy <2044760548@qq.com>
2019-05-15 22:42:36 +0800

    12

---
+++ homework5/Order.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ordertest {
+
+    /// <summary>
+    /// Order class : all orderDetails
+    /// to record each goods and its quantity in this ordering
+    /// </summary>
+    class Order : IComparable {
+
+        private List<OrderDetail> details = new List<OrderDetail>();
+
+        /// <summary>
+        /// Order constructor
+        /// </summary>
+        /// <param name="orderId">order id</param>
+        /// <param name="customer">who orders goods</param>
+        public Order(uint orderId, Customer customer) {
+            Id = orderId;
+            Customer = customer;
+        }
+
+        /// <summary>
+        /// order id
+        /// </summary>
+        public uint Id { get; set; }
+
+        /// <summary>
+        /// the man who orders goods
+        /// </summary>
+        public Customer Customer { get; set; }
+
+
+        public List<OrderDetail> Details {
+            get =>this.details;
+        }
+
+        /// <summary>
+        /// add new orderDetail to order
+        /// </summary>
+        /// <param name="orderDetail">the new orderDetail which will be added</param>
+        public void AddDetails(OrderDetail orderDetail) {
+            if (this.Details.Contains(orderDetail))  {
+                throw new Exception($"orderDetails-{orderDetail.Id} is already existed!");
+            }
+            details.Add(orderDetail);
+        }
+
+        /// <summary>
+        /// remove orderDetail by orderDetailId from order
+        /// </summary>
+        /// <param name="orderDetailId">id of the orderDetail which will be removed</param>
+        public void RemoveDetails(uint orderDetailId) {
+            details.RemoveAll(d =>d.Id==orderDetailId);
+        }
+
+        /// <summary>
+        /// override ToString
+        /// </summary>
+        /// <returns>string:message of the Order object</returns>
+        public override string ToString() {
+            String result = $"orderId:{Id}, customer:({Customer})";
+            details.ForEach(detail => result += "\n\t" + detail);
+            return result;
+        }
+
+        public override bool Equals(object obj) {
+            var o = obj as Order;
+            return o != null && Id == o.Id;
+        }
+
+        public int CompareTo(object obj) {
+            var o = obj as Order;
+            if (o == null) {
+                throw new ArgumentException("Object is not a Order");
+            } else {
+                return this.Id.CompareTo(o.Id);
+            }
+        }
+
+        public double SumOfPrice() {
+            double result = 0;
+            foreach (var detail in details) {
+                result += detail.Goods.Price;
+            }
+            return result;
+        }
+    }
+}
